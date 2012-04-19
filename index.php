96529a2c2a95a85db9708f12d2a340f0e02c0b5c
Pasi Kallinen <paxed@alt.org>
2012-04-19 17:36:25 +0300

    First commit.

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+
+$rgrnpath = '/var/spool/news/rec/games/roguelike/nethack/';
+
+
+$overview = $rgrnpath . '.overview';
+
+print '<html><head>
+
+ <STYLE type="text/css">
+
+ tr.odd { background-color: #f0f0f0; }
+ tr:hover { background-color: #e0e0e0; }
+
+ </STYLE>
+
+
+<title>rec.games.roguelike.nethack</title></head>';
+print '<body>';
+
+if (isset($_GET['num']) && preg_match('/^[0-9]+$/', $_GET['num'])) {
+
+    $article = $rgrnpath . $_GET['num'];
+    $adata = file_get_contents($article);
+
+    print '<pre>';
+    print $adata;
+    print '</pre>';
+
+} else {
+
+    $idxdata = file($overview);
+
+    print '<table>';
+
+    print '<th>Topic</th>';
+    print '<th>Author</th>';
+    print '<th>Date</th>';
+
+    $i = 0;
+
+    foreach ($idxdata as $l) {
+
+	$article = explode("\t", $l);
+
+	if (($i % 2)) {
+	    print '<tr>';
+	} else {
+	    print '<tr class="odd">';
+	}
+
+	print '<td>';
+	print "<a href='?num=".$article[0]."'>".$article[1]."</a>";
+	print '</td>';
+
+	print '<td>';
+	print $article[2];
+	print '</td>';
+
+	print '<td>';
+	print $article[3];
+	print '</td>';
+
+
+	print '<tr>';
+
+	$i++;
+
+    }
+
+    print '</table>';
+
+}
+
+print '</body></html>';
