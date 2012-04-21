8e8f317e2826850dc93c12027fd5f9ba778f6610
Pasi Kallinen <paxed@alt.org>
2012-04-21 23:10:44 +0300

    Silence html validator complaints

--- index.php
+++ index.php
@@ -147,7 +147,11 @@ function show_post($adata, $anum, $smallhead=0)
 }
 
 
+print '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"'.
+      ' "http://www.w3.org/TR/html4/loose.dtd">';
+
 print '<html><head>
+<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <link rel="stylesheet" type="text/css" media="screen" href="newsgroup.css">
 <title>'.$ngname.'</title></head>';
 
